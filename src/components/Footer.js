0b5b2a7a4175ccaa2d680b6babdacf824d2f5dd1
Chris356 <daisukeleblonx@gmail.com>
2020-09-02 12:14:46 +0200

    logo integré avant validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,29 +4,42 @@ export default class Footer extends Component {
     let resumeData = this.props.resumeData;
     return (
       <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  item.name === 'malt' ?
-                    <li key={item.name}>
-                      <a href={item.url} target="blank"><img src={item.image} className={item.name} alt={item.name} /></a>
-                    </li>
-                    :
-                    <li key={item.name}>
-                      <a href={item.url} target="blank"><i className={item.className}></i></a>
-                    </li>
-                )
-              })
-            }
-          </ul>
-          
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
+              {
+                resumeData.socialLinks && resumeData.socialLinks.map((item) => {
+                  return (
+                    item.name === 'malt' ?
+                      <li key={item.name}>
+                        <a href={item.url} target="blank"><img src={item.image} className={item.name} alt={item.name} /></a>
+                      </li>
+                      :
+                      <li key={item.name}>
+                        <a href={item.url} target="blank"><i className={item.className}></i></a>
+                      </li>
+                  )
+                })
+              }
+            </ul>
+            <div className='devContent'>
+              <h4>Ce site a été crée par :</h4>
+              <ul>
+                {
+                  resumeData.devs && resumeData.devs.map((item) => {
+                    return (
+                      <li key={item.name}>
+                        <h5 className='devItem'>{item.name} :<a href={item.url} target="blank"><img src={item.img} className="logodev" alt={item.name} /></a></h5>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
-      </div>
-    </footer>
+      </footer>
     );
   }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,4 +1,5 @@
-
+import logoK from './images/logoK.ico';
+import logoC from './images/logoC.ico'
 let resumeData = {
   "imagebaseurl":"../images/image-background.jpg",
     "name": "Marche Rose à Combourg",
@@ -20,8 +21,8 @@ let resumeData = {
         },
        
       ],
-  "aboutrotary": ["Le rotary est un réseau international de professionnels qui donnent un sens à leurs vies en prônant l’amitié, l’éthique et le service à Autrui.", "Chaque club est spécifique dans ses actions et nous vous invitons à venir découvrir le nôtre.", "Notre club de Combourg - Dol de Bretagne est constitué d’une équipe de 21 personnes de tout horizon professionnel, et de tout âge, se réunissant sur les communes avoisinantes de Combourg tous les Jeudis.", "Ces rendez vous conviviaux, nous permettent d’échanger et de coordonnées nos actions.", "Vous pouvez nous contacter sur notre page Facebook ou Linkdin en message privé."],
-  "marche": ["La Marche Rose prend la suite du semi-marathon organisé par le Rotary Club de Combourg-Dol de Bretagne, il y a 5 ans déjà.", "Organisé en octobre dans le cadre d'Octobre Rose, elle permet, par les droits d'inscriptions et les dons, de soutenir les actions menées contre le Cancer.", "Le choix a été fait d'un partenariat avec la Ligue contre le Cancer, acteur majeur dans ce domaine.", "L'année dernière nous avons pu compter sur une forte mobilisation avec plus de 1 200 participants et nous souhaitons le réitéré lors de cette 4ème édition.", "Vous pourrez ainsi découvrir ou redécouvrir les paysages bucoliques ainsi que le centre ville de Combourg en courant ou en marchant.", "Nous comptons sur vous pour participer à cette belle action et que nous puissions être au moins aussi nombreux cette année.", "Bien entendu, nous avons mis en œuvre des dispositifs afin de respecter les règles en vigueur quant au Covid- 19 et aux distanciations d’usage.", "C’est pourquoi, les coureurs partiront par groupe à partir de 14h30 et les marcheurs les suivront à partir de 15h00 par petit groupe et par tranche de 15 minutes.", "Nous comptons sur vous pour apporter vos masques et les porter lors du départ et arrivée."],
+  "aboutrotary": ["Le Rotary est un réseau international de professionnels qui donnent un sens à leurs vies en prônant l’amitié, l’éthique et le service à Autrui.", "Notre club de Combourg - Dol de Bretagne est constitué d’une équipe de 21 personnes de tout horizon professionnel, des différentes communes avoisinantes et de tout âge, se réunissant sur Combourg ou les communes avoisinantes tous les jeudis, pour un moment convivial et coordonner nos actions.","Chaque club est spécifique dans ses actions et dans l’ambiance que créent ses membres, nous vous invitons à venir découvrir le nôtre qui, nous l’espérons, vous séduira par sa franche camaraderie et la décontraction qui l’accompagne."],
+  "marche": ["La Marche Rose est la continuité du semi-marathon organisé par le Rotary à Combourg, il y a 5 ans maintenant.","La Marche Rose s’adresse à un public plus large composé de coureurs et de marcheurs.Ainsi, tous peuvent profiter du parcours bucolique mis en place pour cet événement.","Ce parcours permet, aux personnes extérieures à Combourg, de profiter du cadre offert par la Ville et ses paysages, et aux Combourgeoises et Combourgeois, de se retrouver pour un moment convivial et engagé.","La Marche Rose est un moment de partage et de forte mobilisation en raison de ses objectifs : collecter le plus grand nombre d’adhésions et de dons afin de soutenir activement les actions menées par la Ligue contre le Cancer.","L’année dernière, nous comptions 1.500 participants.","Nous comptons sur vous pour participer avec force et conviction afin que nous puissions être aussi nombreux cette année.","Bien entendu, nous avons mis en œuvre des dispositifs afin de respecter les règles en vigueur quant à la Covid - 19 et aux distanciations d’usage.","C’est pourquoi, les coureurs partiront par groupe à partir de 14h30 et les marcheurs les suivront à partir de 15h00 par petit groupe et par tranche de 15 minutes.","Nous comptons sur vous pour apporter vos masques et les porter au départ et à l’arrivée du parcours."],
   "ligue": {
     "desc":"N’hésitez pas à vous informer des nombreuses actions de la Ligue contre le Cancer en allant sur",
     "url":"https://www.ligue-cancer.net/article/25989_face-au-cancer-est-plus-forts-ensemble"
@@ -38,59 +39,7 @@ let resumeData = {
     "Pour toute inscription de 10 €, un badge sera offert et à retirer lors de l'événement à l'espace Malouas.",
     "A partir de 50 € de don, vous aurez droit à une attestation à retirer auprès des membres de votre Rotary Club"
   ],
-    "skillsDescription":"Mes compétences",
-    "sponsors":[
-      {
-        "skillname":"HTML5",
-        "logo":"../images/HTML5.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname":"CSS3",
-        "logo":"../images/CSS3.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname":"JavaScript",
-        "logo":"../images/JavaScript.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "React",
-        "logo":"../images/React.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "Node",
-        "logo":"../images/Node.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "MySQL",
-        "logo":"../images/MySQL.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "Python",
-        "logo":"../images/Python.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "Swift",
-        "logo":"../images/Swift.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "Scrum",
-        "logo":"../images/Scrum.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-      {
-        "skillname": "WordPress",
-        "logo":"../images/WordPress.png",
-        "url":"https://cv-christophe-turpin.netlify.app"
-      },
-    ],
+
     "photos":[
       {
         "name":"photo1",
@@ -113,15 +62,17 @@ let resumeData = {
       //   "imgurl":"images/portfolio/phone.jpg"
       // }
     ],
-    "testimonials":[
+    "devs":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "name": "Christophe TURPIN",
+        "img": logoC,
+        "url":"https://cv-christophe-turpin.netlify.app"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
+        "name":"Kenny PHIRI",
+        "img":logoK,
+        "url":""
+      },
     ]
   }
   
